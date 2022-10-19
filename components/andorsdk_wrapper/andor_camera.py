157290c9555183deaa8cdb6c399f3c105802eb2b
Khanh Nguyen <khanhn2@illinois.edu>
2022-10-19 18:15:47 -0500

    Update andor_camera

---
+++ components/andorsdk_wrapper/andor_camera.py
@@ -0,0 +1,93 @@
+import andorsdk
+
+class AndorCamera:
+
+    def __init__(self):
+        print(andorsdk.initialize())
+
+    def getTemperature(self):
+        re = andorsdk.getTemperature()
+        print(re[0])
+        return re[1]
+
+    def getTemperatureRange(self):
+        re = andorsdk.getTemperatureRange()
+        print(re[0])
+        return (re[1], re[2])
+
+    def setTemperature(self, t):
+        re = andorsdk.setTemperature(t)
+        print(re)
+        
+    def turnOffCooler(self):
+        re = andorsdk.turnOffCooler()
+        print(re)
+        
+    def turnOnCooler(self):
+        re = andorsdk.turnOnCooler()
+        print(re)
+        
+    def shutDown(self):
+        re = andorsdk.shutDown()
+        print(re)
+
+    def setReadMode(self, mode=4):
+        """Set the read mode
+        
+        Parameter
+        -------
+        mode: 4 for image mode
+        """
+        re = andorsdk.setReadMode(mode)
+        print(re)
+
+
+    def setExposureTime(self, time):
+        re = andorsdk.setExposureTime(time)
+        print(re)
+
+    def setShutter(self, typ=0, mode=0, closingtime=0, openingtime=0):
+        re = andorsdk.setShutter(typ, mode, closingtime, openingtime)
+        print(re)
+
+    def setAcquisitionMode(self, mode=1):
+        re = andorsdk.setAcquisitionMode(mode)
+        print(re)
+
+    def setImage(self, hbin=2, vbin=2, hstart=1, hend=1024, vstart=1, vend=1024):
+        self.hbin = hbin
+        self.vbin = vbin
+        self.hstart = hstart
+        self.hend = hend
+        self.vstart = vstart
+        self.vend = vend
+        re = andorsdk.setImage(hbin, vbin, hstart, hend, vstart,vend)
+        print(re)
+
+
+    def startAcquisition(self):
+        re = andorsdk.startAcquisition()
+        print(re)
+
+
+    def getStatus(self):
+        """Get the current status
+        """
+        re = andorsdk.getStatus()
+        print(re[0])
+        return re[1]
+
+
+    def getImage(self):
+        """Get the most recent image
+        
+        Returns
+        -------
+        img
+            a np 2d array
+        """
+        
+        re = andorsdk.getImage(self.hbin, self.vbin, self.hstart, self.hend, self.vstart, self.vend)
+        print(re[0])
+
+        return re[1]
--- components/andorsdk_wrapper/andorsdk.pyx
+++ components/andorsdk_wrapper/andorsdk.pyx
@@ -5,133 +5,98 @@ from cython.operator import dereference
 from libc.stdlib cimport malloc, free
 import numpy as np
 
-
-cdef class Andorsdk:
-    hbin=2
-    vbin=2
-    hstart=1
-    hend=1024
-    vstart=1
-    vend=1024
-
-    def __cinit__(self):
-        sta = andorsdk.Initialize(NULL)
-        print(code2msg(sta))
     
-    def getTemperature(self):
-        cdef int * t = <int*> malloc(sizeof(int))
-        sta = andorsdk.GetTemperature(t)
-        print(code2msg(sta))
-        cdef int re = dereference(t)
-        return re
+def initialize():
+    sta = andorsdk.Initialize(NULL)
+    return code2msg(sta)
+
+def getTemperature():
+    cdef int * t = <int*> malloc(sizeof(int))
+    sta = andorsdk.GetTemperature(t)
+    cdef int re = dereference(t)
+    return (code2msg(sta), re)
+
+def getTemperatureRange():
+    cdef int * t1 = <int*> malloc(sizeof(int))
+    cdef int * t2 = <int*> malloc(sizeof(int))
+    sta = andorsdk.GetTemperatureRange(t1, t2)
+    cdef int re1 = dereference(t1)
+    cdef int re2 = dereference(t2)
+    return (code2msg(sta), re1, re2)
+
+def setTemperature(int a):
+    cdef int t = a
+    sta = andorsdk.SetTemperature(t)
+    return code2msg(sta)
     
-    def getTemperatureRange(self):
-        cdef int * t1 = <int*> malloc(sizeof(int))
-        cdef int * t2 = <int*> malloc(sizeof(int))
-        sta = andorsdk.GetTemperatureRange(t1, t2)
-        print(code2msg(sta))
-        cdef int re1 = dereference(t1)
-        cdef int re2 = dereference(t2)
-        return (re1, re2)
-
-    def setTemperature(self, int a):
-        cdef int t = a
-        sta = andorsdk.SetTemperature(t)
-        print(code2msg(sta))
-
-        sta = andorsdk.CoolerON()
-        print(code2msg(sta))
-        
-
-    def turnOffCooler(self):
-        sta = andorsdk.CoolerOFF()
-        print(code2msg(sta))
-        
-
-    def turnOnCooler(self):
-        sta = andorsdk.CoolerON()
-        print(code2msg(sta))
-        
-
-    def shutDown(self):
-        sta = andorsdk.ShutDown()
-        print(code2msg(sta))
-
-    def setReadMode(self, mode=4):
-        """Set the read mode
-        
-        Parameter
-        -------
-        mode: 4 for image mode
-        """
-        sta = andorsdk.SetReadMode(mode)
-        print(code2msg(sta))
-
-
-    def setExposureTime(self, time):
-        sta = andorsdk.SetExposureTime(time)
-        print(code2msg(sta))
-
-    def setShutter(self, typ=0, mode=0, closingtime=0, openingtime=0):
-        sta = andorsdk.SetShutter(typ, mode, closingtime, openingtime)
-        print(code2msg(sta))
-
-    def setAcquisitionMode(self, mode=1):
-        sta = andorsdk.SetAcquisitionMode(mode)
-        print(code2msg(sta))
-
-    def setImage(self, hbin=2, vbin=2, hstart=1, hend=1024, vstart=1, vend=1024):
-        self.hbin = hbin
-        self.vbin = vbin
-        self.hstart = hstart
-        self.hend = hend
-        self.vstart = vstart
-        self.vend = vend
-        sta = andorsdk.SetImage(hbin, vbin, hstart,hend, vstart,vend)
-        print(code2msg(sta))
-
-
-    def startAcquisition(self):
-        sta = andorsdk.StartAcquisition()
-        print(code2msg(sta))
-
-    
-    def getStatus(self):
-        """Get the current status
-        """
-        cdef int * t = <int*> malloc(sizeof(int))
-        sta = andorsdk.GetStatus(t)
-        print(code2msg(sta))
-
-        cdef int re = dereference(t)
-        return code2msg(re)
+def turnOffCooler():
+    sta = andorsdk.CoolerOFF()
+    return code2msg(sta)
     
 
-    def getImage(self):
-        """Get the most recent image
-        
-        Returns
-        -------
-        img
-            a np 2d array
-        """
-        height = (self.vend - self.vstart + 1)/self.vbin
-        width = (self.hend - self.hstart + 1)/self.hbin
+def turnOnCooler():
+    sta = andorsdk.CoolerON()
+    return code2msg(sta)
+    
 
-        cdef at_u32 size = height*width
-        cdef at_32 * t = <at_32*> malloc(sizeof(at_32)*size)
-        
-        sta = andorsdk.GetMostRecentImage(t, size)
-        print(code2msg(sta))
+def shutDown():
+    sta = andorsdk.ShutDown()
+    return code2msg(sta)
 
-        re = []
-        for i in range(0, height):
-            row = []
-            for j in range(0, width):
-                row.append(t[i*width + j])
-            re.append(row)
-        
-        re = np.array(re)
-        re = np.flip(re, 0)
+def setReadMode(mode=4):
+    sta = andorsdk.SetReadMode(mode)
+    return code2msg(sta)
 
-        return re
+
+def setExposureTime(time):
+    sta = andorsdk.SetExposureTime(time)
+    return code2msg(sta)
+
+def setShutter(typ=0, mode=0, closingtime=0, openingtime=0):
+    sta = andorsdk.SetShutter(typ, mode, closingtime, openingtime)
+    return code2msg(sta)
+
+def setAcquisitionMode(mode=1):
+    sta = andorsdk.SetAcquisitionMode(mode)
+    return code2msg(sta)
+
+def setImage(hbin=2, vbin=2, hstart=1, hend=1024, vstart=1, vend=1024):
+    sta = andorsdk.SetImage(hbin, vbin, hstart, hend, vstart,vend)
+    return code2msg(sta)
+
+
+def startAcquisition():
+    sta = andorsdk.StartAcquisition()
+    return code2msg(sta)
+
+
+def getStatus():
+    cdef int * t = <int*> malloc(sizeof(int))
+    sta = andorsdk.GetStatus(t)
+    cdef int re = dereference(t)
+    return (code2msg(sta), code2msg(re))
+
+
+def getImage(hbin=2, vbin=2, hstart=1, hend=1024, vstart=1, vend=1024):
+    height = (vend - vstart + 1)/vbin
+    width = (hend - hstart + 1)/hbin
+
+    cdef at_u32 size = height*width
+    cdef at_32 * t = <at_32*> malloc(sizeof(at_32)*size)
+    
+    sta = andorsdk.GetMostRecentImage(t, size)
+    
+    if (code2msg(sta) != "DRV_SUCCESS"):
+        return (code2msg(sta), 0)
+
+    re = []
+    for i in range(0, height):
+        row = []
+        for j in range(0, width):
+            row.append(t[i*width + j])
+        re.append(row)
+    
+    re = np.array(re)
+    re = np.flip(re, 0)
+
+    return (code2msg(sta), re)
