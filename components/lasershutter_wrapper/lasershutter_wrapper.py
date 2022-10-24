3ac7eed838263f31aa548a248cec57c6afe62849
Khanh Nguyen <khanhn2@illinois.edu>
2022-10-24 15:56:50 -0500

    update lasershutter

--- components/lasershutter_wrapper/lasershutter_wrapper.py
+++ components/lasershutter_wrapper/lasershutter_wrapper.py
@@ -3,22 +3,35 @@ import sys
 import os
 import zugbruecke
 from zugbruecke import CtypesSession
-import zugbruecke.ctypes as ctypes
 import pythonnet
 import numpy as np
 
-ctypes64 = CtypesSession(arch = 'win64')
-PiUsb = ctypes64.windll.LoadLibrary('PiUsb.dll')
+ctypes = CtypesSession(arch = 'win64')
+PiUsb = ctypes.windll.LoadLibrary('PiUsb.dll')
 
 class LaserShutter:
     def __init__(self, serial=527):
-        a = ctypes.c_int()
+        PiUsb.piConnectShutter.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int] 
+        PiUsb.piConnectShutter.restype = ctypes.c_void_p
+        a = ctypes.c_int(2)
         
         PiUsb.piConnectShutter(ctypes.byref(a), ctypes.c_int(serial))
         print(a)
     
+    def find(self):
+        # piFindShutters(int* DeviceCount, int* SerialNumberArray, int ArraySize)
+        PiUsb.piFindShutters.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int), ctypes.c_int] 
+        PiUsb.piFindShutters.restype = ctypes.c_int
+        a = ctypes.c_int(2)
+        b = ctypes.c_int(2)
+        
+        c = PiUsb.piFindShutters(ctypes.byref(a), ctypes.byref(b), ctypes.c_int(1))
+        print(c)
+        print(a)
+        print(b)
+
+
     def setState(self, serial=1):
         i = c_int()
         b = PiUsb.piSetShutterState(1, 527)
         print(a)
-    
